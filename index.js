affdce262198341e21c4dc31762e8e77958bc45d
Murari Yadav <murari.yadav@thingmzing.com>
2022-12-23 14:58:36 +0530

    bottomTab text persist

--- index.js
+++ index.js
@@ -1,60 +1,15 @@
 import {Navigation} from 'react-native-navigation';
 import registerAppScreens from './src/navigation/register-screens';
-import ROUTE_NAME from './src/navigation/navigation-constants';
+import {setRoute} from './src/navigation/app-root';
 
 registerAppScreens(); // register all the screens
 
-Navigation.events().registerAppLaunchedListener(() =>
-  Navigation.setRoot({
-    root: {
-      bottomTabs: {
-        id: 'BOTTOM_TABS_LAYOUT',
-        children: [
-          {
-            stack: {
-              id: 'HOME_TAB',
-              children: [
-                {
-                  component: {
-                    id: 'HOME_SCREEN',
-                    name: ROUTE_NAME.APP_HOME,
-                  },
-                },
-              ],
-              options: {
-                bottomTab: {
-                  text: 'Home',
-                  //icon: require('./home.png'),
-                },
-              },
-            },
-          },
-          {
-            stack: {
-              id: 'SEARCH_TAB',
-              children: [
-                {
-                  component: {
-                    id: 'SEARCH_SCREEN',
-                    name: ROUTE_NAME.APP,
-                  },
-                },
-              ],
-              options: {
-                bottomTab: {
-                  text: 'Search',
-                  // icon: require('./profile.png'),
-                },
-              },
-            },
-          },
-        ],
-        options: {bottomTabs: {borderColor: 'red', borderWidth: 10}},
-      },
-    },
-  }),
-);
+Navigation.events().registerAppLaunchedListener(() => setRoute());
 
-Navigation.mergeOptions('BOTTOM_TABS_LAYOUT', {
-  bottomTab: {selectedTextColor: 'blue'},
+Navigation.setDefaultOptions({
+  topBar: {visible: false},
+  bottomTabs: {
+    selectedIconColor: 'blue',
+    titleDisplayMode: 'alwaysShow',
+  },
 });
